cbdb29d19654c91c8f147ccdb48e5843e52d549c
asteriskzuo <zuoyuhsywn@hotmail.com>
2023-11-14 10:59:23 +0800

    docs: update

--- src/biz/GiftMessageList/types.tsx
+++ src/biz/GiftMessageList/types.tsx
@@ -3,11 +3,24 @@ export type GiftMessageListItemData = {
    * suggestion: seqId('_gf').toString()
    */
   id: string;
+  /**
+   * The avatar of the sender.
+   */
   avatar?: string;
+  /**
+   * The nickname of the sender.
+   */
   nickName: string;
+  /**
+   * The content of the message.
+   */
   content: string;
+  /**
+   * The icon of the gift.
+   */
   giftIcon: string;
   /**
+   * The count of the gift.
    * Default number 1.
    */
   giftCount?: number;
